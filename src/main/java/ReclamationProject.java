3e79eba52bcb3058409a5c997e0fe2940142e841
SthMax <SthMax@wirelessprv-10-193-51-13.near.illinois.edu>
2017-09-26 20:15:33 -0500

    Reclamed.

--- src/main/java/ReclamationProject.java
+++ src/main/java/ReclamationProject.java
@@ -7,20 +7,39 @@
  * 4. Add comments and Javadoc comments where needed
  * 5. Remove unnecessary comments as appropriate
  */
+/**
+ * Just for Reclamation.
+ */
+public class ReclamationProject {
 
-public class ReclamationProject
-{
-    static String doit(String a,String b){
-        if (a.length() > b.length()){
-            String c = a; // TODO: set c to a
-            a=b; b=c;}
-        String r = (a.equals(b)) ? "" : ""; // I love the ternary operator!
+    /**
+     *This method received something and output someting.
+     * @param a input String a
+     * @param b input String b
+     * @return value r
+     */
+    static String doit(final String a, final String b) {
+        String shortOne, longOne;
+        if (a.length() > b.length()) {
+            shortOne = b;
+            longOne = a;
+        } else {
+            longOne = a;
+            shortOne = b;
+        }
+        String r = "";
         /*
          * For loop with i
          */
-        for (int i = 0; i < a.length(); i++) { for (int j = a.length() - i; j > 0; j--) {
-                for (int k = 0; k < b.length()- j; k++) {
-                    r = (a.regionMatches(i, b, k, j) && j >r.length()) ? a.substring(i,i + j) : r; // Do it!
-                        }} // Ah yeah
-        } return r; }
+        for (int i = 0; i < shortOne.length(); i++) {
+            for (int j = shortOne.length() - i; j > 0; j--) {
+                for (int k = 0; k < longOne.length() - j; k++) {
+                    if (shortOne.regionMatches(i, longOne, k, j) && j > r.length()) {
+                        r = shortOne.substring(i, i + j);
+                    }
+                }
+            }
+        }
+        return r;
+    }
 }
